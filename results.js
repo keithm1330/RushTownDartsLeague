a8beb6ae9285bed398cfbaf86443156709b9c6ef
keithm1330 <keith13@live.ie>
2025-11-15 01:54:17 +0000

    Create results.js

---
+++ results.js
@@ -0,0 +1,7 @@
+// results.js
+const results = [
+    { team: "Team A", won: 4, draw: 1, lost: 0 },
+    { team: "Team B", won: 3, draw: 0, lost: 2 },
+    { team: "Team C", won: 1, draw: 2, lost: 2 },
+    { team: "Team D", won: 0, draw: 1, lost: 4 }
+];
