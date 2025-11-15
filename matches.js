2a7decaa38af88a871285a4182651ee5cec36a86
keithm1330 <keith13@live.ie>
2025-11-15 02:18:31 +0000

    Create matches.js

---
+++ matches.js
@@ -0,0 +1,6 @@
+const matches = [
+  { date: "14-11-2025", teamA: "Cilles B", scoreA: 7, teamB: "Rivervalley", scoreB: 1 },
+  { date: "14-11-2025", teamA: "Round Towers", scoreA: 1, teamB: "Cilles A", scoreB: 7 },
+  { date: "14-11-2025", teamA: "Strand", scoreA: 6, teamB: "Top Shop", scoreB: 2 }
+  
+];
