6e0f151e8b2ac41b3a79b11a279d7f41a1658e76
keithm1330 <keith13@live.ie>
2025-11-15 02:01:49 +0000

    Update results.js

--- results.js
+++ results.js
@@ -1,7 +1,10 @@
 // results.js
 const results = [
-    { team: "Team A", won: 4, draw: 1, lost: 0 },
-    { team: "Team B", won: 3, draw: 0, lost: 2 },
-    { team: "Team C", won: 1, draw: 2, lost: 2 },
-    { team: "Team D", won: 0, draw: 1, lost: 4 }
+    { team: "Cilles A", won: 1, draw: 0, lost: 0 },
+    { team: "Cilles B", won: 1, draw: 0, lost: 0 },
+    { team: "Rivervalley", won: 0, draw: 0, lost: 1 },
+    { team: "Top Shop", won: 0, draw: 0, lost: 1},
+    { team: "Strand", won: 1, draw: 0, lost: 0},
+     { team: "Cricket", won: 0, draw: 0, lost: 0 },
+     { team: "Round Towers", won: 0, draw: 0, lost: 1 },
 ];
