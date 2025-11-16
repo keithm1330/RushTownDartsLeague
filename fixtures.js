62bc3cab2da0e8dbc6f38d5fc671fee126161be3
keithm1330 <keith13@live.ie>
2025-11-16 01:45:10 +0000

    Create fixtures.js

---
+++ fixtures.js
@@ -0,0 +1,236 @@
+[
+  {
+    "date": "21-Nov",
+    "match": "Round Towers vs Top Shop Dart Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "21-Nov",
+    "match": "The Strand Darts Team vs Cricket Club Darts Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "21-Nov",
+    "match": "River Valley Darts Team vs Cilles A Darts Team",
+    "venue": "Rivervalley Community Centre",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "28-Nov",
+    "match": "Cricket Club Darts Team vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "28-Nov",
+    "match": "The Strand Darts Team vs Cilles B Darts Team",
+    "venue": "The Strand Bar Rush",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "28-Nov",
+    "match": "River Valley Darts Team vs Round Towers",
+    "venue": "Round Towers Lusk GAA",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "09-Jan",
+    "match": "Round Towers vs Cilles B Darts Team",
+    "venue": "Round Towers Lusk GAA",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "09-Jan",
+    "match": "The Strand Darts Team vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "09-Jan",
+    "match": "Cricket Club Darts Team vs Top Shop Dart Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "16-Jan",
+    "match": "River Valley Darts Team vs Cricket Club Darts Team",
+    "venue": "Rivervalley Community Centre",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "16-Jan",
+    "match": "The Strand Darts Team vs Round Towers",
+    "venue": "The Strand Bar Rush",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "16-Jan",
+    "match": "Cilles B Darts Team vs Top Shop Dart Team",
+    "venue": "Cilles B Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "23-Jan",
+    "match": "River Valley Darts Team vs The Strand Darts Team",
+    "venue": "Rivervalley Community Centre Bar",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "23-Jan",
+    "match": "Top Shop Dart Team vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "23-Jan",
+    "match": "Cilles B Darts Team vs Cricket Club Darts Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "30-Jan",
+    "match": "Cilles B Darts Team vs Cilles A Darts Team",
+    "venue": "Cilles B Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "30-Jan",
+    "match": "River Valley Darts Team vs Top Shop Dart Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "30-Jan",
+    "match": "Round Towers vs Cricket Club Darts Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Feb",
+    "match": "River Valley Darts Team vs Cilles B Darts Team",
+    "venue": "Rivervalley Community Centre Bar",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Feb",
+    "match": "Round Towers vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Feb",
+    "match": "The Strand Darts Team vs Top Shop Dart Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "13-Feb",
+    "match": "River Valley Darts Team vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "13-Feb",
+    "match": "Round Towers vs Top Shop Dart Team",
+    "venue": "Round Towers Lusk GAA",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "13-Feb",
+    "match": "The Strand Darts Team vs Cricket Club Darts Team",
+    "venue": "The Strand Bar Rush",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Feb",
+    "match": "The Strand Darts Team vs Cilles B Darts Team",
+    "venue": "Cilles B Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Feb",
+    "match": "River Valley Darts Team vs Round Towers",
+    "venue": "Rivervalley Community Centre Bar",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Feb",
+    "match": "Cricket Club Darts Team vs Cilles A Darts Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Feb",
+    "match": "Cricket Club Darts Team vs Top Shop Dart Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Feb",
+    "match": "Round Towers vs Cilles B Darts Team",
+    "venue": "Cilles B Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Feb",
+    "match": "The Strand Darts Team vs Cilles A Darts Team",
+    "venue": "The Strand Bar Rush",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Mar",
+    "match": "Cilles B Darts Team vs Top Shop Dart Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Mar",
+    "match": "River Valley Darts Team vs Cricket Club Darts Team",
+    "venue": "Rush Cricket Club",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "06-Mar",
+    "match": "The Strand Darts Team vs Round Towers",
+    "venue": "Round Towers Lusk GAA",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Mar",
+    "match": "Cilles B Darts Team vs Cricket Club Darts Team",
+    "venue": "Cilles B Home",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Mar",
+    "match": "River Valley Darts Team vs The Strand Darts Team",
+    "venue": "The Strand Bar Rush",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "20-Mar",
+    "match": "Top Shop Dart Team vs Cilles A Darts Team",
+    "venue": "The Top Shop",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Mar",
+    "match": "River Valley Darts Team vs Top Shop Dart Team",
+    "venue": "Rivervalley Community Centre Bar",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Mar",
+    "match": "Round Towers vs Cricket Club Darts Team",
+    "venue": "Round Towers Lusk GAA",
+    "link": "LINK_HERE"
+  },
+  {
+    "date": "27-Mar",
+    "match": "Cilles B Darts Team vs Cilles A Darts Team",
+    "venue": "Cilles A Home",
+    "link": "LINK_HERE"
+  }
+]
